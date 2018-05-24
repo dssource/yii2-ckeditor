65b536cece3eb4bf973c51cf286f1dbccabed73d
Алексей <support@digital-solution.ru>
2018-05-24 13:46:20 +0300

    Update Assets.php

--- Assets.php
+++ Assets.php
@@ -4,12 +4,12 @@
  * Time: 1:06
  */
 
-namespace mihaildev\ckeditor;
+namespace dssource\ckeditor;
 
 use yii\web\AssetBundle;
 
 class Assets extends AssetBundle{
-	public $sourcePath = '@mihaildev/ckeditor/editor';
+	public $sourcePath = '@dssource/ckeditor/editor';
 
     public $js = [
         'ckeditor.js',
